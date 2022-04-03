c204120f2f3324a99ea2cfa52e4931af5a29eacf
Jader Raniere Albrez <jader.ralbrez@gmail.com>
2022-04-03 00:53:13 -0400

    Script 1

---
+++ script.js
@@ -0,0 +1,35 @@
+async function pegaProdutos(){
+    return fetch("https://mystique-v2-americanas.juno.b2w.io/autocomplete?content=camiseta&source=nanook").then(result => result.json());
+}
+
+async function carregaProdutos(){
+    listaProdutos = await pegaProdutos();
+    var divResultados = document.getElementById('resultados-busca');
+    listaProdutos.products.forEach(produto => {
+        divResultados.innerHTML += `<div name="produto" class="produto ocultar"><p>${produto.name}</p><p>${produto.id}</p><p>${produto.type}</p></div>`
+    });
+}
+
+function filtraProdutos(){
+    var palavraChave = document.getElementById('caixaBusca').value;
+    if(palavraChave == null || palavraChave == ""){
+        document.getElementById('resultados-busca').innerHTML += `<p class="mensagemErro">Por favor, insira uma palavra chave.</p>`
+    } else {
+        let listaProdutos = document.getElementsByName('produto');
+        var contaProduto = 0;
+        listaProdutos.forEach(item =>{
+            if(item.textContent.includes(palavraChave)){
+                item.classList.remove('ocultar');
+                contaProduto += 1;
+            } else{
+                item.classList.add('ocultar');
+               }
+        })
+
+        if(contaProduto == 0){
+            document.getElementById('resultados-busca').innerHTML = `<p class="mensagemErro">Não foram encontrados resultados para sua busca.</p>` 
+        }
+        
+    } 
+}
+
